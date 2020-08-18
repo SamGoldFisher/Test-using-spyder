8fcea96f13e79533cfbca98c2021c423ebc638a2
Sam <tibo.cadiou@live.fr>
2020-08-18 17:00:34 +0200

    Initializing a coding using spyder and gitkraken
    I just tried to code using spyder and commit using gitkraken into my git repositroy to check if it worked

---
+++ Main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 18 16:11:12 2020
+
+@author: CADIOU
+"""
+
+print("I love using python and gitkraken")
+
+for i in range(10):
+    print(i)
+    
+print("This obviously do work and that is amazing ;-)")    
